ae87cb0f3931cf5b58063750569346ec98356238
Tonit93 <141759801+Tonit93@users.noreply.github.com>
2024-11-24 23:35:12 +0200

    main-b

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -4,9 +4,9 @@ import '../Header/style.scss';
 const UserMenu = () => {
     return ( 
     <div className="user-menu">
-        <div className="user-menu__home"></div>
+        <div className="user-menu__home">1234</div>
         <div className="user-menu__local">Глобус</div>
-        <div className="user-menu__user"></div>
+        <div className="user-menu__user">Чет там еще</div>
     </div> 
     );
 }
